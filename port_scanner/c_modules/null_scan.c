f8cffda715ae12b5c83cefd6f5569fbd58c08fcf
InGyu <yangik11@naver.com>
2024-11-25 16:08:27 +0900

    NULL을 0으로 대체함

--- port_scanner/c_modules/null_scan.c
+++ port_scanner/c_modules/null_scan.c
@@ -6,7 +6,7 @@ void start_null_scan(const char *src_ip, int src_port, const char *dst_ip, int d
     int sockfd = create_raw_socket();
     set_socket_options(sockfd);
     char packet[PACKET_SIZE];
-    create_raw_packet(packet, src_ip, src_port, dst_ip, dst_port, NULL);
+    create_raw_packet(packet, src_ip, src_port, dst_ip, dst_port, 0);
     send_raw_packet(sockfd, packet, dst_ip, dst_port);
     receive_fin_null_xmas_response(sockfd, src_port, time_limit);
     close(sockfd);
