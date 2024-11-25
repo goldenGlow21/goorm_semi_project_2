52f892f14b94eacd27ac3d5ac48a8e7f9a9d63da
InGyu <yangik11@naver.com>
2024-11-25 15:47:46 +0900

    tcp_connect.c 오탈자 수정

--- port_scanner/c_modules/tcp_connect.c
+++ port_scanner/c_modules/tcp_connect.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-void start_connect_scan(const char *src_ip, int src_port, const char *dst_ip, int dst_port, int time_limit)
+void start_connect_scan(const char *dst_ip, int dst_port)
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in dst_addr;
@@ -12,7 +12,7 @@ void start_connect_scan(const char *src_ip, int src_port, const char *dst_ip, in
     dst_addr.sin_family = AF_INET;
     dst_addr.sin_port = htons(dst_port);
     dst_addr.sin_addr.s_addr = inet_addr(dst_ip);
-    if (connect(sockfd, (struct sockaddr *)&dst_addr, sizeof(dst_addr)) == -1)
+    if (connect(sockfd, (struct sockaddr *)&dst_addr, sizeof(dst_addr)) == 0)
         printf("포트 열림(tcp connection 성공) %s:%d\n", dst_ip, dst_port);
     else
         printf("포트 닫힘(tcp connection 실패) %s:%d", dst_ip, dst_port);
@@ -22,5 +22,5 @@ void start_connect_scan(const char *src_ip, int src_port, const char *dst_ip, in
 
 int main()
 {
-    start_connect_scan("192.168.79.11", 10000, "192.168.79.3", 8080, 3);
+    start_connect_scan("192.168.79.3", 8080);
 }
