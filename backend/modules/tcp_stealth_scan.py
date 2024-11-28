cd33f3c88406f478f9cd993cc711abc0634b3c2a
yang-ingyu <yangik11@naver.com>
2024-11-28 12:51:51 +0900

    멀티스레딩 미완료

--- backend/modules/tcp_stealth_scan.py
+++ backend/modules/tcp_stealth_scan.py
@@ -1,44 +1,59 @@
 # 양인규 로컬에서 테스트 함 추가 테스트 필요함
-from scapy.all import sr1, conf #from scapy.all import IP, TCP, sr1, conf
-from scapy.layers.inet import IP, TCP
+import socket
 import sys
 import time
+import random
+import os
+import threading
+from common import get_port
+from concurrent.futures import ThreadPoolExecutor
+from scapy.all import sr1, conf #from scapy.all import IP, TCP, sr1, conf
+from scapy.layers.inet import IP, TCP
 
+lock = threading.Lock()
 # FIN null xmas 스캔 함수
-def stealth_scan(flag, target, ports):
-    print(f"{target} ip에서 {ports} 포트 스캔")
+def stealth_scan(flag, target, port):
+
     conf.verb = 0  # 디버깅 메시지 0이면 출력 안함 1이면 출력 함
-    open_ports = []
+    random_port = get_port()
+    # stealth 패킷 생성
+    if flag == "F":
+        packet = IP(dst=target) / TCP(sport=random_port, dport=port, flags="F") # / ip와 tcp를 묶는 연산자
+    elif flag == "N":
+        packet = IP(dst=target) / TCP(sport=random_port, dport=port)
+    elif flag == "X":
+        packet = IP(dst=target) / TCP(sport=random_port, dport=port, flags="FPU")
+    # 패킷 전송 및 응답 수신 sr1함수는 패킷을 전송하고 첫 번째 응답을 기다리는 함수
+    response = sr1(packet, timeout=1)
+    if response is None:  # 응답x == 포트 열림
+        return port
+    time.sleep(0) # 속도 조절 시 사용할 것
 
-    for port in ports:
-        # stealth 패킷 생성
-        if flag == "F":
-            packet = IP(dst=target) / TCP(dport=port, flags="F") # / ip와 tcp를 묶는 연산자
-        elif flag == "N":
-            packet = IP(dst=target) / TCP(dport=port)
-        elif flag == "X":
-            packet = IP(dst=target) / TCP(dport=port, flags="FPU")
-        # 패킷 전송 및 응답 수신 sr1함수는 패킷을 전송하고 첫 번째 응답을 기다리는 함수
-        response = sr1(packet, timeout=1)
 
-
-        if response is None:  # 응답x == 포트 열림
-            open_ports.append(port)
-
-        time.sleep(0) # 속도 조절 시 사용할 것
-
+def multi_stealth_scan(flag, target, start_port, end_port):
+    start = time.time()
+    random_ports = list(range(start_port, end_port+1))
+    time.sleep(1)
+    random.shuffle(random_ports)
+    with ThreadPoolExecutor(max_workers=os.cpu_count() * 2) as executor:
+        results = list(executor.map(lambda port: stealth_scan(flag, target, port), random_ports))
+    open_ports = list(filter(None, results)) # 필터링된 포트
+    open_ports.sort()
+    end = time.time()
+    print(f"{end - start:.5f} sec")
     return open_ports
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print("Usage: python tcp_stealth_scan.py <type> <target> <port1,port2,port3,...>")
+        print("Usage: python tcp_stealth_scan.py <type> <target> <start_port> <end_port>")
         sys.exit(1)
 
     flag = sys.argv[1]
     target_ip = sys.argv[2]
-    port_list = list(map(int, sys.argv[3].split(',')))
+    start_port = int(sys.argv[3])
+    end_port = int(sys.argv[4])
 
-    open_ports = stealth_scan(flag, target_ip, port_list)
+    open_ports = multi_stealth_scan(flag, target_ip, start_port, end_port)
 
     if open_ports:
         print(f"포트 열림 : {open_ports}")
