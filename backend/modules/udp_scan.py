e7765139ca889fba0e06cfbc3687a010ae258ec8
yang-ingyu <yangik11@naver.com>
2024-11-28 17:02:03 +0900

    udp 쓰레드 완료

--- backend/modules/udp_scan.py
+++ backend/modules/udp_scan.py
@@ -3,37 +3,51 @@ from scapy.all import sr1, conf #from scapy.all import IP, TCP, sr1, conf
 from scapy.layers.inet import IP, UDP, ICMP
 import sys
 import time
+import random
+import os
+from concurrent.futures import ThreadPoolExecutor
 
 # UDP 스캔 함수
-def udp_scan(target, ports):
-    print(f"{target} ip에서 {ports} 포트 스캔")
+def udp_scan(target, port):
     conf.verb = 0  # 디버깅 메시지 0이면 출력 안함 1이면 출력 함
-    open_or_filtered_ports = []
 
-    for port in ports:
-        # UDP 패킷 생성
-        packet = IP(dst=target) / UDP(dport=port) # / ip와 tcp를 묶는 연산자
-        # 패킷 전송 및 응답 수신 sr1함수는 패킷을 전송하고 첫 번째 응답을 기다리는 함수
-        response = sr1(packet, timeout=1)
+    # UDP 패킷 생성
+    packet = IP(dst=target) / UDP(dport=port) # / ip와 tcp를 묶는 연산자
+    # 패킷 전송 및 응답 수신 sr1함수는 패킷을 전송하고 첫 번째 응답을 기다리는 함수
+    response = sr1(packet, timeout=1)
 
-        if response is None:  # 응답x == 열려 있거나 닫혀 있음
-            open_or_filtered_ports.append(port)
+    if response is None:  # 응답x == 열려 있음
+        return port
+    elif response.haslayer(ICMP) and response[ICMP].type == 3: # 응답o == 닫혀잇음
+        return
+    time.sleep(0) # 속도 조절 시 사용할 것
 
-        time.sleep(0) # 속도 조절 시 사용할 것
 
-    return open_or_filtered_ports
+def multi_udp_scan(target, start_port, end_port):
+    start = time.time()
+    random_ports = list(range(start_port, end_port+1))
+    time.sleep(1)
+    random.shuffle(random_ports)
+    with ThreadPoolExecutor(max_workers=os.cpu_count() * 2) as executor:
+        results = list(executor.map(lambda port: udp_scan(target, port), random_ports))
+    open_ports = list(filter(None, results)) # 필터링된 포트
+    open_ports.sort()
+    end = time.time()
+    print(f"{end - start:.5f} sec")
+    return open_ports
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python udp_scan.py <target> <port1,port2,port3,...>")
+        print("Usage: python udp_scan.py <target> <start_port> <end_port>")
         sys.exit(1)
 
     target_ip = sys.argv[1]
-    port_list = list(map(int, sys.argv[2].split(',')))
+    start_port = int(sys.argv[2])
+    end_port = int(sys.argv[3])
 
-    open_ports = udp_scan(target_ip, port_list)
+    ports = multi_udp_scan(target_ip, start_port, end_port)
 
-    if open_ports:
-        print(f"포트 열림 또는 필터링 됨 : {open_ports}")
+    if ports:
+        print(f"포트 열림 : {ports}")
     else:
         print("포트 닫힘")
