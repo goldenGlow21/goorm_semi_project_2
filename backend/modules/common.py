b553a9bcb386bc065eda48cc54b4797c9c52bb4a
yang-ingyu <yangik11@naver.com>
2024-11-29 15:01:24 +0900

    URL를 IP주소로 변환하는 함수 추가

--- backend/modules/common.py
+++ backend/modules/common.py
@@ -1,7 +1,7 @@
-
-from contextlib import closing
 import socket
 import random
+from contextlib import closing
+from urllib.parse import urlparse
 
 def get_port():
     max_try = 10
@@ -15,4 +15,10 @@ def get_port():
                 print(f"다른 소켓으로 사용 시도..")
         print("사용가능한 포트를 찾지 못했습니다.")
 
-
+def get_ip_from_domain(url):
+    try:
+        domain = urlparse(url).netloc
+        ip_address = socket.gethostbyname(domain)
+        return ip_address
+    except Exception as e:
+        print("URL로 IP주소를 찾지 못했습니다." + str(e))
