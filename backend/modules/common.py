4329309f49daa4024c3917f3f6e3049277925391
yang-ingyu <yangik11@naver.com>
2024-11-29 17:51:59 +0900

    도메인으로 수정

--- backend/modules/common.py
+++ backend/modules/common.py
@@ -15,10 +15,9 @@ def get_available_port():
                 print(f"다른 소켓으로 사용 시도..")
         print("사용가능한 포트를 찾지 못했습니다.")
 
-def get_ip_from_domain(url):
+def get_ip_from_domain(domain):
     try:
-        domain = urlparse(url).netloc
         ip_address = socket.gethostbyname(domain)
         return ip_address
     except Exception as e:
-        print("URL로 IP주소를 찾지 못했습니다." + str(e))
+        print("도메인로 IP주소를 찾지 못했습니다." + str(e))
