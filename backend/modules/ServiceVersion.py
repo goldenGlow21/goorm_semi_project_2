20141794e936d815e20f661131e728253fd8bd69
goldenGlow21 <jonghoon8928@gmail.com>
2024-12-03 13:13:17 +0900

    fix: module import for Service VerVersion

--- backend/modules/ServiceVersion.py
+++ backend/modules/ServiceVersion.py
@@ -5,7 +5,7 @@ import requests
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(file), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from os import cpu_count
 from typing import Dict, Optional, List
 from concurrent.futures import ThreadPoolExecutor
