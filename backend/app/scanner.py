1ed09b41ae3c62e40102a58e37ab900dd9cbe723
goldenGlow21 <jonghoon8928@gmail.com>
2024-12-04 13:07:43 +0900

    fix: log config

--- backend/app/scanner.py
+++ backend/app/scanner.py
@@ -52,10 +52,7 @@ def scan_ports(ip: str, start_port: int, end_port: int, scan_type: str):
 
             # 리스트를 딕셔너리로 변환
             if isinstance(results, list):
-                return {
-                    "open_ports": results,  # 열린 포트 리스트
-                    "total_ports_scanned": len(results)  # 스캔된 포트 개수
-                }
+                return {"port_list": results}
             else:
                 # 예외적인 반환값 처리
                 raise RuntimeError("Unexpected result format from scan function")
